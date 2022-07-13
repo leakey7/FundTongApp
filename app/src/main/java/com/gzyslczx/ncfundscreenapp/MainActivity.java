f3926cf8ed4d7ca55171bbe7854c7ff6a1068a4d
leakey <leakey_9677@163.com>
2022-07-13 14:24:35 +0800

    Init

---
+++ app/src/main/java/com/gzyslczx/ncfundscreenapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gzyslczx.ncfundscreenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
